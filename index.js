d9cd12a79bd1ad87ec71500ae15b7ecddf0477e4
Fátima PB <20221024@uthh.edu.mx>
2024-07-15 21:12:12 -0600

    arreglando las imagenes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 const multer = require('multer'); // Importar multer
 require("dotenv").config();
 const AdministradoresRoutes = require("./src/routes/Administradores");
@@ -17,7 +18,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
-app.use('/uploads', express.static('uploads')); // Servir archivos estáticos de la carpeta uploads
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/api',AdministradoresRoutes);
 app.use('/api' , CategoriaRoutes);
 app.use('/api', NivelRoutes);
